5d4c8a5a5f84b879469c1074cfc6b60b6a5933f4
Josef Samsel <josefsamsel@Josefs-MacBook-Pro.local>
2024-11-19 11:19:14 +0100

    kumo is a cloud, ho

---
+++ script.js
@@ -0,0 +1,15 @@
+function Joeclicked(){
+    alert("Button Clicked");
+    document.body.style.backgroundColor = "Red";
+
+    const color1 = "red"
+    const color2 = "green"
+
+    if (document.body.style.backgroundColor == color1){
+        document.body.style.backgroundColor = color2;
+    } else {
+        document.body.style.backgroundColor == color1
+
+    }
+} 
+
